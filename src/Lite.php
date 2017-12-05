f660787ec331873fa4e3cf640ceb2653c9072f90
小小酥 <smsjs@163.com>
2017-12-05 19:54:26 +0800

    优化代码
    优化代码

--- src/Lite.php
+++ src/Lite.php
@@ -20,9 +20,9 @@ class Lite {
   }
 
   //视频缩略图
-  public function get_video_image($ffmpegPath, $input, $output, $fromdurasec = '01')
+  public function get_video_image($input, $output, $fromdurasec = '00:00:01')
   {
-    $command = "$ffmpegPath -i $input -an -ss 00:00:$fromdurasec -r 1 -vframes 1 -f mjpeg -y $output";
+    $command = $this->config['ffmpeg_path']." -i ".$input." -an -ss ".$fromdurasec." -r 1 -vframes 1 -f mjpeg -y ".$output;
     if(exec($command)){
       return true;
     };
@@ -30,19 +30,19 @@ class Lite {
   }
 
   //视频缩略图
-  public function get_video_image_gif($ffmpegPath, $input, $output, $fromdurasec = '01')
+  public function get_video_image_gif($input, $output, $fromdurasec = '00:00:01', $second = 5)
   {
-    $command = "$ffmpegPath -i $input -an -ss 00:00:$fromdurasec -r 1 -vframes 5 -y $output";
+    $command = $this->config['ffmpeg_path']." -i ".$input." -an -ss ".$fromdurasec." -r 1 -vframes ".$second." -y ".$output;
     if(exec($command)){
       return true;
     };
     return false;
   }
 
-  //截取视频前5秒
-  public function get_video_part($ffmpegPath, $input, $output, $begin_second = '01', $end_second = '05')
+  //截取视频前n秒
+  public function get_video_part($input, $output, $begin_second = '00:00:01', $end_second = '00:00:05')
   {
-    $command = "$ffmpegPath -ss 00:00:$begin_second -i $input -t 00:00:$end_second $output";
+    $command = $this->config['ffmpeg_path']." -ss ".$begin_second." -i ".$input." -t ".$end_second." ".$output;
     if(exec($command)){
       return true;
     };
@@ -50,9 +50,9 @@ class Lite {
   }
 
   //获取视频的时长
-  public function get_video_timeline($ffmpegpath, $input)
+  public function get_video_timeline($input)
   {
-    $command = "$ffmpegpath -i ".$input." 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//";
+    $command = $this->config['ffmpeg_path']." -i ".$input." 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//";
     $timeline = exec($command);
     $time_arr = explode(':', $timeline);
     $timeline = $time_arr[0] * 3600 + $time_arr[1] * 60 + intval($time_arr[2]);
@@ -60,32 +60,32 @@ class Lite {
   }
 
   //获取视频的详细信息
-  public function get_video_info($ffmpegpath, $input)
+  public function get_video_info($input)
   {
-    $command = $ffmpegpath. " -v quiet -print_format json -show_format -show_streams " .$input;
+    $command = $this->config['ffprobe_path']. " -v quiet -print_format json -show_format -show_streams " .$input;
     exec($command,$out,$status);
     $new = implode('',$out);
     return json_decode($new, true);
   }
 
   //处理视频格式--转为h264格式
-  public function mobile_video_codec($ffmpegPath, $input, $output)
+  public function mobile_video_codec($input, $output)
   {
-    $command = $ffmpegPath.' -y -i '.$input.' -vcodec -threads 2 libx264 -metadata:s:v:0 rotate=0 '.$output;
+    $command = $this->config['ffmpeg_path']." -y -i ".$input." -vcodec -threads 2 libx264 -metadata:s:v:0 rotate=0 ".$output;
     exec($command);
   }
 
   //将mp4转为完整的ts
-  public function video_to_ts($ffmpegpath, $input, $output)
+  public function video_to_ts($input, $output)
   {
-    $command = $ffmpegpath. " -i ".$input." -c copy -bsf h264_mp4toannexb " .$input;
+    $command = $this->config['ffmpeg_path']." -i ".$input." -c copy -bsf h264_mp4toannexb " .$input;
     exec($command);
   }
 
   //将ts切片，并生成m3u8文件
-  public function video_to_m3u8_and_ts($ffmpegpath, $input, $outputM3u8, $outputTs)
+  public function video_to_m3u8_and_ts($input, $outputM3u8, $outputTs)
   {
-    $command = $ffmpegpath. " -i ".$input." -c copy -map 0 -f segment -segment_list ".$outputM3u8." -segment_time 10 " .$outputTs;
+    $command = $this->config['ffmpeg_path']." -i ".$input." -c copy -map 0 -f segment -segment_list ".$outputM3u8." -segment_time 10 " .$outputTs;
     exec($command);
   }
 }
